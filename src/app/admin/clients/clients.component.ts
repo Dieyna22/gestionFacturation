22e53bdcbc45aa2ed4613ed81b1303406c04bba9
dieyna22 <senedieynaba87@gmail.com>
2024-08-02 13:52:10 +0000

    update Client

--- src/app/admin/clients/clients.component.ts
+++ src/app/admin/clients/clients.component.ts
@@ -6,6 +6,7 @@ import { Report } from 'notiflix/build/notiflix-report-aio';
 import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-clients',
@@ -220,6 +221,7 @@ currentClient: any;
 // Methode pour charger les infos du zone  à modifier
 chargerInfosClient(paramClient:any){
  this.currentClient = paramClient;
+ console.log(paramClient);
  this.inputnom = paramClient.nom_client;
  this.inputprenom = paramClient.prenom_client;
  this.inputmail = paramClient.email_client;
@@ -322,7 +324,8 @@ get totalPages(): number {
 showChampsAddressPrincipal: boolean=false;
 showChampsAddressLivraison: boolean=false;
 showChampsOther: boolean=false;
-
+currentStep = 1;
+isFileValid = false;
 afficherChampsAddressPrincipal(){
  this.showChampsAddressPrincipal=!this.showChampsAddressPrincipal;
 }
@@ -335,6 +338,111 @@ afficherChampsAddressLivraison(){
   this.showChampsOther=!this.showChampsOther;
  }
 
+ goToNextStep() {
+  this.currentStep++;
+}
 
+downloadModel() {
+  // Création du modèle de données
+  const modelData = [
+    {  prenom: '', nom: '', Nom_entreprise: '', type_client: '', Statut_client: '' , email_client: '', adress_client: '', num_id_fiscal: '', 
+    Adresse_Code_postal : '', ville_client: '', pays_client: '', tel_client : '', noteInterne_client : '', nom_destinataire: '', pays_livraison: '',
+    ville_livraison : '', code_postal_livraison : '', tel_destinataire : '', email_destinataire: '',  infoSupplemnt : '',
+    }
+  ];
+
+  // Création de la feuille de calcul
+  const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(modelData);
+
+  // Définition d'une largeur uniforme pour toutes les colonnes
+  const columnWidth = 20; // Largeur en caractères
+
+ 
+    // Application de la largeur à toutes les colonnes utilisées
+    if (worksheet['!ref']) {
+      const range = XLSX.utils.decode_range(worksheet['!ref']);
+      const columnCount = range.e.c + 1; // Nombre de colonnes
+
+      worksheet['!cols'] = Array(columnCount).fill({ wch: columnWidth });
+    } else {
+      // Fallback si !ref n'est pas défini (ce qui est peu probable dans ce cas)
+      const columnCount = Object.keys(modelData[0]).length;
+      worksheet['!cols'] = Array(columnCount).fill({ wch: columnWidth });
+    }
+
+
+  // Création du classeur
+  const workbook: XLSX.WorkBook = { 
+    Sheets: { 'Clients': worksheet }, 
+    SheetNames: ['Clients'] 
+  };
+
+  // Conversion du classeur en buffer
+  const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+  // Sauvegarde du fichier
+  this.saveAsExcelFile(excelBuffer, "modele_ajoutClients");
+}
+
+private saveAsExcelFile(buffer: any, fileName: string): void {
+  // Création d'un Blob à partir du buffer
+  const data: Blob = new Blob([buffer], {
+    type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
+  });
+
+  // Création d'un lien de téléchargement
+  const link: HTMLAnchorElement = document.createElement('a');
+  link.href = window.URL.createObjectURL(data);
+  link.download = `${fileName}.xlsx`;
+
+  // Simulation du clic pour déclencher le téléchargement
+  link.click();
+
+  // Nettoyage
+  setTimeout(() => {
+    window.URL.revokeObjectURL(link.href);
+    link.remove();
+  }, 100);
+}
+
+ private file: File | null = null;
+ modelHeaders: string[] = [];
+ 
+
+ onFileChange(event: any) {
+  this.file = event.target.files[0];
+  this.isFileValid = true; 
+}
+
+uploadFile() {
+  if (this.file) {
+    const formData = new FormData();
+    formData.append('file', this.file, this.file.name);
+
+    this.http.post('http://127.0.0.1:8000/api/importClient', formData).subscribe(
+      response => {this.goToNextStep(); this.listeClients()},
+      error => console.error('Erreur d\'importation', error)
+    );
+  }
+  
+}
+
+exportExcel() {
+  this.clientService.exportToExcel().subscribe(
+    (data: Blob) => {
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'export.xlsx';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    },
+    (err)=> {
+      console.log(err);
+    }
+    
+  );
+}
 
 }
