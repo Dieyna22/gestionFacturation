78353e625016544b282bfcb75058ab8933eeb616
dieyna22 <senedieynaba87@gmail.com>
2024-12-25 17:42:12 +0000

    corrections

--- src/app/services/apiUrl.ts
+++ src/app/services/apiUrl.ts
@@ -1,3 +1,3 @@
-// export let apiUrl = 'http://127.0.0.1:8000/api'
-export let apiUrl = 'https://api.yokoo.pro/api'
+export let apiUrl = 'http://127.0.0.1:8000/api'
+// export let apiUrl = 'https://api.yokoo.pro/api'
 
